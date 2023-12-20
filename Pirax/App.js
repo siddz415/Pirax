e847532056dfb01290cd765e6b94d991ba022b9e
Sidd <siddz415@gmail.com>
2023-12-19 20:57:16 -0800

    added signup page

--- Pirax/App.js
+++ Pirax/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { Login, Home, Welcome } from "./screens";
+import { Login, Home, Welcome, Signup } from "./screens";
 
 const Stack = createNativeStackNavigator();
 
@@ -21,6 +21,13 @@ export default function App() {
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{
+            headerShown: false
+          }}
+              />
+        <Stack.Screen
+          name="Signup"
+          component={Signup}
           options={{
             headerShown: false
           }}
