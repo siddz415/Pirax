7a2420ca9fa5cc74367fbbd534afe39a8c502f24
Sidd <siddz415@gmail.com>
2023-12-19 20:52:44 -0800

    added code in welcome.js

---
+++ Pirax/screens/Welcome.js
@@ -0,0 +1,92 @@
+import { View, Text, Pressable, Image } from 'react-native'
+import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import COLORS from '../constants/colors';
+import Button from '../components/Button';
+
+const Welcome = ({ navigation }) => {
+
+    return (
+        <LinearGradient
+            style={{
+                flex: 1
+            }}
+            colors={[COLORS.secondary, COLORS.primary]}
+        >
+            <View style={{ flex: 1 }}>
+                <View>
+                    <Image
+                        source={require("../assets/pirax.png")}
+                        style={{
+                            height: 200,
+                            width: 200,
+                            borderRadius: 20,
+                            position: "absolute",
+                            top: 110,
+                            left: 100,
+                            transform: [
+                                { translateX: 50 },
+                                { translateY: 50 },
+                                { rotate: "-15deg" }
+                            ]
+                        }}
+                    />
+                </View>
+
+                {/* content  */}
+
+                <View style={{
+                    paddingHorizontal: 22,
+                    position: "absolute",
+                    top: 400,
+                    width: "100%"
+                }}>
+                    <Text style={{
+                        fontSize: 50,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Let's Get</Text>
+                    <Text style={{
+                        fontSize: 46,
+                        fontWeight: 800,
+                        color: COLORS.white
+                    }}>Started</Text>
+                    </View>
+
+                    <Button
+                        title="Join Now"
+                        onPress={() => navigation.navigate("Signup")}
+                        style={{
+                            marginTop: 22,
+                            width: "100%"
+                        }}
+                    />
+
+                    <View style={{
+                        flexDirection: "row",
+                        marginTop: 12,
+                        justifyContent: "center"
+                    }}>
+                        <Text style={{
+                            fontSize: 16,
+                            color: COLORS.white
+                        }}>Already have an account ?</Text>
+                        <Pressable
+                            onPress={() => navigation.navigate("Login")}
+                        >
+                            <Text style={{
+                                fontSize: 16,
+                                color: COLORS.white,
+                                fontWeight: "bold",
+                                marginLeft: 4
+                            }}>Login</Text>
+                        </Pressable>
+
+                    </View>
+                </View>
+            
+        </LinearGradient>
+    )
+}
+
+export default Welcome
